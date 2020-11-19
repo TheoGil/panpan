d905ea82a100b3905b1abcbf15af0c46f03cd52d
= <=>
2020-11-19 09:44:40 +0100

    add explanatory dialogue for mobile users

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,4 +1,5 @@
 import { debounce } from "throttle-debounce";
+import A11yDialog from "a11y-dialog";
 import GL from "./GL";
 
 const BP = Number.parseInt(
@@ -12,9 +13,14 @@ class App {
     console.clear();
 
     this.onResize = debounce(DEBOUNCE_AMOUNT, this.onResize.bind(this));
+    this.mobileDialog = new A11yDialog(
+      document.getElementById("my-accessible-dialog")
+    );
 
     if (window.innerWidth > BP) {
       this.initGL();
+    } else {
+      this.mobileDialog.show();
     }
 
     window.addEventListener("resize", this.onResize);
@@ -36,11 +42,21 @@ class App {
         this.gl.resurect();
       }
 
+      // If necessary hide dialog explaining that mobile won't feature the animation
+      if (this.mobileDialog.shown) {
+        this.mobileDialog.hide();
+      }
+
       this.gl.onResize();
     } else {
       // If GL was existing and be go below BP, kill GL
       if (this.gl && this.gl.alive) {
         this.gl.kill();
+
+        // If necessary show dialog to explain that mobile won't feature the animation
+        if (!this.mobileDialog.shown) {
+          this.mobileDialog.show();
+        }
       }
     }
   }
