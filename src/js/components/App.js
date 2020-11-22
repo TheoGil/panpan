214a7fb93ca20f512ab7be966f91cf84d3c65af4
= <=>
2020-11-22 09:54:40 +0100

    remove obsolete breakpoint JS handling

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,10 +2,6 @@ import { debounce } from "throttle-debounce";
 import A11yDialog from "a11y-dialog";
 import GL from "./GL";
 
-const BP = Number.parseInt(
-  window.getComputedStyle(document.querySelector('meta[name="breakpoint"]'))
-    .fontSize
-);
 const DEBOUNCE_AMOUNT = 200;
 const RESIZE_HEIGHT_THRESHOLD = 100;
 
